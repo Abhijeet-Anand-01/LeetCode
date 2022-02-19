f18c1817c49bb99ec8fcfabe09a223051c4d574a
Abhijeet Anand <abhijeetad500@gmail.com>
2022-02-20 02:18:17 +0530

    Hard 41 solved

---
+++ Java/Hard/41. First Missing Positive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+		while (i < nums.length) {
+            int correctIndex = nums[i] - 1;
+			if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correctIndex]) {
+				int temp = nums[correctIndex];
+				nums[correctIndex] = nums[i];
+				nums[i] = temp;
+			} 
+			else {
+				i++;
+			}
+		}
+        
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                return index + 1;
+            }
+        }
+        
+		return nums.length + 1;
+    }
+}

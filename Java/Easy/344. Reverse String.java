414bd3e0e5b630e93caa84478454b04c8833c8c3
Abhijeet Anand <abhijeetad500@gmail.com>
2022-02-26 01:10:30 +0530

    Recursion 344

--- Java/Easy/344. Reverse String.java
+++ Java/Easy/344. Reverse String.java
@@ -1,13 +1,15 @@
 class Solution {
     public void reverseString(char[] s) {
-        reverseRecurssion(s, 0);
+        reverseRecursion(s, 0);
     }
     
-    private void reverseRecurssion(char[] s, int index){
-        if(index < s.length) {
-            char temp = s[index];
-            reverseRecurssion(s, index + 1);
-            s[s.length - 1  - index] = temp;
+    private void reverseRecursion(char[] s, int index){
+        if(index == s.length) {
+            return;
         }
+        char temp = s[index];
+        reverseRecursion(s, index + 1);
+        s[s.length - 1  - index] = temp;
+        
     }
 }

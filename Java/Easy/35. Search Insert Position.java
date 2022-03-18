dc31fdef7a37d8950f7133f17cc15d3fbc9eb9ec
Abhijeet Anand <abhijeetad500@gmail.com>
2022-03-18 13:07:02 +0530

    Update 35

--- Java/Easy/35. Search Insert Position.java
+++ Java/Easy/35. Search Insert Position.java
@@ -2,10 +2,9 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int index = -1;
         while (++index < nums.length) {
-            if (target == nums[index])
-                break;
-            if (target < nums[index])
-                break;
+            if (target <= nums[index]) {
+                return index;
+            }
         }
         return index;
     }

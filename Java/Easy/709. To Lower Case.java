a621d09c88233d8450fac4078cf6ba288a12d029
Abhijeet Anand <abhijeetad500@gmail.com>
2022-03-21 18:19:30 +0530

    Update 709. To Lower Case.java

--- Java/Easy/709. To Lower Case.java
+++ Java/Easy/709. To Lower Case.java
@@ -12,10 +12,10 @@ class Solution {
 class Solution {
     public String toLowerCase(String s) {
         for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
-				s = s.replace(s.charAt(i), (char)(s.charAt(i) + 32));
-			}
-		}
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                s = s.replace(s.charAt(i), (char)(s.charAt(i) + 32));
+            }
+        }
         return s;
     }
 }

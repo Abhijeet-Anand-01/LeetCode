3559b418427317ee2a92aa4be98449e4afccab55
Abhijeet Anand <abhijeetad500@gmail.com>
2022-03-22 23:58:31 +0530

    Update 989. Add to Array-Form of Integer.java

--- Java/Easy/989. Add to Array-Form of Integer.java
+++ Java/Easy/989. Add to Array-Form of Integer.java
@@ -6,8 +6,8 @@ class Solution {
         while (--n >= 0 || k > 0) {
             if (n >= 0)
                 k += num[n];
-                ans.add(k % 10);
-                k /= 10;
+            ans.add(k % 10);
+            k /= 10;
         }
         Collections.reverse(ans);
         return ans;

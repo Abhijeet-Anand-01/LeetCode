a1a2172c60457f37cb63110ed986a2f5be18c3a1
Abhijeet Anand <abhijeetad500@gmail.com>
2022-03-23 14:10:35 +0530

    Create 1217. Minimum Cost to Move Chips to The Same Position.java

---
+++ Java/Easy/1217. Minimum Cost to Move Chips to The Same Position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0, even = 0;
+        for(int ele : position) {
+            if(ele % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        return Math.min(odd, even);
+    }
+}

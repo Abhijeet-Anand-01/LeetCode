e95bfd327cb2cd0ecb0c4eadecf85179289ae1e5
Abhijeet Anand <abhijeetad500@gmail.com>
2022-03-24 11:55:49 +0530

    Update 1539. Kth Missing Positive Number.java

--- Java/Easy/1539. Kth Missing Positive Number.java
+++ Java/Easy/1539. Kth Missing Positive Number.java
@@ -18,7 +18,7 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         int start = 0, end = arr.length;
         while (start < end) {
-            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
             if (arr[mid] - 1 - mid < k)
                 start = mid + 1;
             else

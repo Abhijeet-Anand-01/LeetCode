808791f41a5738f626bf300d058f27b0c5e92035
Abhijeet Anand <abhijeetad500@gmail.com>
2022-03-25 00:15:41 +0530

    Create 217. Contains Duplicate.java

---
+++ Java/Easy/217. Contains Duplicate.java
@@ -0,0 +1,29 @@
+// Approach 1
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int ind = 1; ind < nums.length; ind++) {
+            if(nums[ind] == nums[ind - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+// Approach 2
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> distinct = new HashSet<Integer>();
+        for(int num : nums) {
+            if(distinct.contains(num)) {
+                return true;
+            }
+            distinct.add(num);
+        }
+        return false;
+    }
+}

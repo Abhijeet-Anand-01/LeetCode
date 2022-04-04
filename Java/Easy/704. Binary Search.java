9c45286409038a49e2bb24339f5493223973806c
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-04 17:15:58 +0530

    Update 704. Binary Search.java

--- Java/Easy/704. Binary Search.java
+++ Java/Easy/704. Binary Search.java
@@ -1,3 +1,5 @@
+// Approach 1 : Looping
+
 class Solution {
     public int search(int[] nums, int target) {
         int start = 0, end = nums.length - 1;
@@ -14,5 +16,30 @@ class Solution {
             }
         }
         return -1;
+    }
+}
+
+
+// Approach 2 : Recursion
+
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+    
+    private int binarySearch(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        
+        int mid = start + (end - start) / 2;
+        if (target == nums[mid]) {
+            return mid;
+        }
+        else if (target < nums[mid]) {
+            return binarySearch(nums, target, start, mid - 1);
+        }
+        else return binarySearch(nums, target, mid + 1, end);
+        
     }
 }

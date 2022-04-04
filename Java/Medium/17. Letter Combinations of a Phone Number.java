68ef20597065dd6338ee6a04106a1aa619ee0c27
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-04 16:26:48 +0530

    Update 17. Letter Combinations of a Phone Number.java

--- Java/Medium/17. Letter Combinations of a Phone Number.java
+++ Java/Medium/17. Letter Combinations of a Phone Number.java
@@ -1,35 +1,36 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        return possibleCombinations("",digits);
+        return possibleCombinations("", digits);
     }
     
     private ArrayList<String> possibleCombinations(String p, String up){
         if (up.isEmpty()) {
-			      ArrayList<String> list = new ArrayList<>();
-			      if (!p.isEmpty()) {
-				        list.add(p);
-			  }
-			  return list;
-		}
+            ArrayList<String> list = new ArrayList<>();
+            if (!p.isEmpty()) {
+                list.add(p);
+            }
+            return list;
+        }
 
         int digit = up.charAt(0) - '0';
         int start = (digit - 2) * 3;
         int end = (digit - 1) * 3;
-        if (digit == 7 || digit == 8) {
+        if (digit == 7) {
             end += 1;
         }
-        if (digit == 8 || digit == 9) {
+        if (digit == 8) {
             start += 1;
+            end += 1;
         }
         if (digit == 9) {
+            start += 1;
             end += 2;
         }
-      
         ArrayList<String> ans = new ArrayList<>();
         for (int i = start; i < end; i++) {
             char ch = (char) ('a' + i);
-            ans.addAll(actualpadList(p + ch, up.substring(1)));
+            ans.addAll(possibleCombinations(p + ch, up.substring(1)));
         }
-		    return ans;
+        return ans;
     }
 }

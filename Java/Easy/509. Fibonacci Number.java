a55d9ee67379e5ab8f25c7cad31ff20e54e475be
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-04 17:38:53 +0530

    Update 509. Fibonacci Number.java

--- Java/Easy/509. Fibonacci Number.java
+++ Java/Easy/509. Fibonacci Number.java
@@ -1,3 +1,5 @@
+// Approach 1 : Looping
+
 class Solution {
     public int fib(int n) {
         int num1 = 0, num2 = 1, sum = 0;  
@@ -7,5 +9,19 @@ class Solution {
 			num2 = sum;
         }
         return num1;
+    }
+}
+
+
+// Approach 2 : Recursion
+
+class Solution {
+    public int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        
+        return fib(n - 1) + fib(n - 2);
     }
 }

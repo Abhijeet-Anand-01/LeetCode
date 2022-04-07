6a79639d994e2761da06519a6fc9ac0931c876e1
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-07 16:09:44 +0530

    Update 46. Permutations.java

--- Java/Medium/46. Permutations.java
+++ Java/Medium/46. Permutations.java
@@ -1,3 +1,5 @@
+// Approach 1
+
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
@@ -20,4 +22,33 @@ class Solution {
             } 
         }
     } 
+}
+
+
+// Approach 2 
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> finalResult = new ArrayList<>();
+        backtrack(nums, finalResult, new ArrayList<>(), new boolean[nums.length]);
+        return finalResult;
+    }
+
+    private void backtrack(int[] nums, List<List<Integer>> finalResult, List<Integer> currResult, boolean[] used) {
+
+        if (currResult.size() == nums.length) {
+            finalResult.add(new ArrayList<>(currResult));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])
+                continue;
+            currResult.add(nums[i]);
+            used[i] = true;
+            backtrack(nums, finalResult, currResult, used);
+            used[i] = false;
+            currResult.remove(currResult.size() - 1);
+        }
+    }
 }

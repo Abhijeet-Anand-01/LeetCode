a0e9bf7857fafc0d7ea7a08bfd15b7860844003d
Abhijeet Anand <abhijeetad500@gmail.com>
2022-04-12 00:30:02 +0530

    Create 494. Target Sum.java

---
+++ Java/Medium/494. Target Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    int count = 0;
+    public int findTargetSumWays(int[] nums, int target) {
+		sumRecursion(nums, target, 0, 0);
+		return count;
+	}
+
+	public void sumRecursion(int[] nums, int target, int index, int sum) {
+		if (index == nums.length) {
+			if (sum == target) {
+				count++;
+			}
+			return;
+		}
+		sumRecursion(nums, target, index + 1, sum + nums[index]);
+		sumRecursion(nums, target, index + 1, sum - nums[index]);
+	}
+}

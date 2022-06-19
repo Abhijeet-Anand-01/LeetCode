8d0ee23708db2f956e2d497ee722d5828f19dbd3
Abhijeet Anand <abhijeetad500@gmail.com>
2022-06-19 22:57:41 +0530

    Update 62. Unique Paths.java

--- Java/Medium/62. Unique Paths.java
+++ Java/Medium/62. Unique Paths.java
@@ -1,10 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        return uniquePathsHelper(m, n, new int[m + 1][n + 1]);
+        return uniquePathsHelper(m - 1, n - 1 , new int[m][n]);
     }
 
     private int uniquePathsHelper(int m, int n, int[][] memo) {
-        if (m == 1 || n == 1) 
+        if (m == -1 || n == -1) 
+            return 0;
+        
+        if (m == 0 && n == 0)
             return 1;
         
         if (memo[m][n] != 0) 

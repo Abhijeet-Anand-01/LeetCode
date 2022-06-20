318ad3c5a27d808179700f1f5855ebd74d71e9e1
Abhijeet Anand <abhijeetad500@gmail.com>
2022-06-20 19:23:57 +0530

    Update 509. Fibonacci Number.java

--- Java/Easy/509. Fibonacci Number.java
+++ Java/Easy/509. Fibonacci Number.java
@@ -25,3 +25,24 @@ class Solution {
         return fib(n - 1) + fib(n - 2);
     }
 }
+
+
+// Approach 3 : Dynamic Programming
+
+class Solution {
+    public int fib(int n) {
+        int[] memo = new int[n + 1];
+        return dp(n, memo);
+    }
+    
+    private int dp(int n, int[] memo) {
+        if (n == 0 || n == 1)
+            return n;
+        
+        if (memo[n] != 0)
+            return memo[n];
+        
+        memo[n] = dp(n - 1, memo) + dp(n - 2, memo);
+        return memo[n];
+    }
+}

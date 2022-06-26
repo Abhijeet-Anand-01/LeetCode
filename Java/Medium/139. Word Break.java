0a014cbf98bd467cf7b72bfd5a60a38e89104cee
Abhijeet Anand <abhijeetad500@gmail.com>
2022-06-26 16:18:46 +0530

    Update 139. Word Break.java

--- Java/Medium/139. Word Break.java
+++ Java/Medium/139. Word Break.java
@@ -1,22 +1,23 @@
 class Solution {
+
     HashMap<String, Boolean> memo = new HashMap<String, Boolean>();
     public boolean wordBreak(String s, List<String> wordDict) {
         if (s.length() == 0)
-			return true;
+            return true;
 
-		if (memo.containsKey(s))
-			return memo.get(s);
+        if (memo.containsKey(s))
+            return memo.get(s);
 
-		for (int i = 0; i < wordDict.size(); i++) {
-			if (s.indexOf(wordDict.get(i)) == 0) {
-				String suffix = s.substring(wordDict.get(i).length());
-				if (wordBreak(suffix, wordDict)) {
-					memo.put(s, true);
-					return true;
-				}
-			}
-		}
-		memo.put(s, false);
-		return false;
+        for (String word : wordDict) {
+            if (s.indexOf(word) == 0) {
+                String suffix = s.substring(word.length());
+                if (wordBreak(suffix, wordDict)) {
+                    memo.put(s, true);
+                    return true;
+                }
+            }
+        }
+        memo.put(s, false);
+        return false;
     }
 }

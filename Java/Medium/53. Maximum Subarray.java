53f7ad199e3ee1b4f672447dd280cfd97c7a28b8
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-04 00:26:08 +0530

    Update 53. Maximum Subarray.java

--- Java/Medium/53. Maximum Subarray.java
+++ Java/Medium/53. Maximum Subarray.java
@@ -1,3 +1,5 @@
+// Approach 1 : Brute Force
+
 class Solution {
     public int maxSubArray(int[] nums) {
         int sum = 0, max = nums[0];
@@ -10,6 +12,25 @@ class Solution {
             if (sum < 0)
                 sum = 0;
         }
+        return max;
+    }
+}
+
+
+// Approach 2 : DP
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n]; //dp[i] means the maximum subarray ending with A[i];
+        int max = dp[0] = nums[0];;
+        
+        for(int i = 1; i < n; i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+        
         return max;
     }
 }

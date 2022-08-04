bb941411d5408bb2307d12eb7a843b102a66c6ae
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-04 18:53:04 +0530

    Update 724. Find Pivot Index.java

--- Java/Easy/724. Find Pivot Index.java
+++ Java/Easy/724. Find Pivot Index.java
@@ -1,15 +1,11 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int total = 0, sum = 0;
-        for (int num : nums) {
-            total += num;
+        int sum = 0, leftsum = 0;
+        for (int x : nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (sum * 2 == total - nums[i]) {
-                return i;
-            }
-            sum += nums[i];
-        }   
-        return -1;  
+        return -1;
     }
 }

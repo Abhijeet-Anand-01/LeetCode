bf99c6edae1af890d2db4ea9f895bf453a96aab4
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-04 11:20:17 +0530

    Update 338. Counting Bits.java

--- Java/Easy/338. Counting Bits.java
+++ Java/Easy/338. Counting Bits.java
@@ -1,3 +1,5 @@
+// Approach 1 : Bit Manipulation
+
 class Solution {
     public int[] countBits(int n) {
         int[] num = new int[n + 1];
@@ -5,5 +7,22 @@ class Solution {
             num[i] = num[i >> 1] + (i & 1);
         }
         return num;
+    }
+}
+
+
+// Approach 2 : DP
+
+class Solution {
+    public int[] countBits(int num) {
+        int result[] = new int[num + 1];
+        int offset = 1;
+        for (int index = 1; index < num + 1; ++index){
+            if (offset * 2 == index){
+                offset *= 2;
+            }
+            result[index] = result[index - offset] + 1;
+        }
+        return result;
     }
 }

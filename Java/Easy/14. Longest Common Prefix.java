0636c262acf70c3afd1b1c734a18d1006f162b0c
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-05 16:33:02 +0530

    Update 14. Longest Common Prefix.java

--- Java/Easy/14. Longest Common Prefix.java
+++ Java/Easy/14. Longest Common Prefix.java
@@ -1,11 +1,8 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null) {
-            return "";
-        }
+    
         String prefix = strs[0];
-        int i = 0;
-        while (++i < strs.length) {
+        for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
             }

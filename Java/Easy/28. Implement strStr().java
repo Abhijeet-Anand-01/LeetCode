97c52f0c13926023eecd2c086d5a236ca30f3d77
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-05 13:49:18 +0530

    Update 28

--- Java/Easy/28. Implement strStr().java
+++ Java/Easy/28. Implement strStr().java
@@ -1,4 +1,4 @@
-// Approach 1 
+// Approach 1 : Not recommended
 
 class Solution {
     public int strStr(String haystack, String needle) {
@@ -11,12 +11,10 @@ class Solution {
 
 class Solution {
     public int strStr(String haystack, String needle) {
-        int l1 = haystack.length(), l2 = needle.length();
-        if (needle == null) {
-            return 0;
-        }
-        for (int i = 0; i <= l1 - l2; i++) {
-            if (haystack.substring(i, i+l2).equals(needle)) {
+        
+        int len1 = haystack.length(), len2 = needle.length();    
+        for (int i = 0; i < len1 - len2 + 1; i++) {
+            if (haystack.substring(i, i + len2).equals(needle)) {
                 return i;
             }
         }

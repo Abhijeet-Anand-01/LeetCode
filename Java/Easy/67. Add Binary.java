be785e4e44202b7831bf2e0ab45b603bfedda4b0
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-08 16:27:07 +0530

    Update 67. Add Binary.java

--- Java/Easy/67. Add Binary.java
+++ Java/Easy/67. Add Binary.java
@@ -1,13 +1,19 @@
 class Solution {
     public String addBinary(String a, String b) {
-        String sb = "";
-        int carry = 0;
-        for (int i = 0; i < Math.max(a.length(), b.length()) || carry != 0; i++) {
-            int ia = (i < a.length()) ? a.charAt(a.length() - i - 1) - '0' : 0;
-            int ib = (i < b.length()) ? b.charAt(b.length() - i - 1) - '0' : 0;
-            sb = ((ia + ib + carry) % 2) + sb;
+        StringBuilder sb = new StringBuilder();
+        int carry = 0, lenA = a.length(), lenB = b.length();
+        for (int i = 0; i < lenA || i < lenB; i++) {
+            int ia = (i < lenA) ? a.charAt(lenA - i - 1) - '0' : 0;
+            int ib = (i < lenB) ? b.charAt(lenB - i - 1) - '0' : 0;
+            // sb.insert(0, (ia + ib + carry) % 2);            // Insert takes O(n) : Not recommended to use here in this case bcoz it will give O(n^2)
+            sb.append((ia + ib + carry) % 2);                  // Append takes O(1) 
             carry = (ia + ib + carry) / 2;
         }
-        return sb;
+        if (carry != 0) 
+            sb.append(carry);
+            // sb.insert(0, carry);
+        
+        return sb.reverse().toString();
+        // return sb.toString();
     }
 }

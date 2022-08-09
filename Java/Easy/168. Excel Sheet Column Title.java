c3ad121931ba200d000765511e6b666080db7398
Abhijeet Anand <abhijeetad500@gmail.com>
2022-08-09 22:43:06 +0530

    Update 168. Excel Sheet Column Title.java

--- Java/Easy/168. Excel Sheet Column Title.java
+++ Java/Easy/168. Excel Sheet Column Title.java
@@ -1,8 +1,8 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder result = new StringBuilder();
-        while (columnNumber-- > 0) {
-            result.append((char)('A' + columnNumber % 26));
+        while (columnNumber > 0) {
+            result.append((char)('A' + --columnNumber % 26));
             columnNumber /= 26;
         }
         result.reverse();

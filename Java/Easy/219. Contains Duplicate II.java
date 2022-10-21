4c9dad0e723d56412d78e8506f51ab565319880d
Abhijeet Anand <abhijeetad500@gmail.com>
2022-10-21 20:30:36 +0530

    Update 219. Contains Duplicate II.java

--- Java/Easy/219. Contains Duplicate II.java
+++ Java/Easy/219. Contains Duplicate II.java
@@ -1,9 +1,27 @@
+// Approach 1 : Brute force O(n * k)
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length && j <= i + k; j++) {
                 if (nums[i] == nums[j]) return true;
             }
+        }
+        return false;
+    }
+}
+
+
+// Approach 2 : HashMap O(n)
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (i - map.get(nums[i]) <= k) return true;
+            }
+            map.put(nums[i], i);
         }
         return false;
     }

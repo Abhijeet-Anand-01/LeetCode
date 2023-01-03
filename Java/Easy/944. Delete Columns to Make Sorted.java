f0fb18cb73c08cc7243e508fbccb972fec8da6c8
Abhijeet Anand <abhijeetad500@gmail.com>
2023-01-03 21:23:39 +0530

    Easy String 944

---
+++ Java/Easy/944. Delete Columns to Make Sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 0; j < strs.length - 1; j++) {
+                if (Character.compare(strs[j].charAt(i), strs[j + 1].charAt(i)) > 0) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
